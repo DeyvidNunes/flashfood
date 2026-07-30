0723869a16cbd5ff9685714f05b41758d514b640
Deyvid <deyvidnunes99@gmail.com>
2026-07-30 15:51:20 -0300

    feat: cria entidade Usuario e pacote model

---
+++ src/main/java/com/flashfood/flashfood/FlashfoodApplication.java
@@ -0,0 +1,13 @@
+package com.flashfood.flashfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashfoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flashfood/flashfood/FlashfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashfood.flashfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
